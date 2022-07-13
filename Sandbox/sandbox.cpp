ccbd1065858bdf7379c2306676aaaf1bd67b29fe
sarpsenturk <sarpsenturl38@gmail.com>
2022-07-14 02:38:04 +0300

    Add spdlog

--- Sandbox/sandbox.cpp
+++ Sandbox/sandbox.cpp
@@ -1,9 +1,11 @@
 #include "orion/orion.h"
 
+#include <spdlog/spdlog.h>
+
 class Sandbox : public orion::Application
 {
-    void on_user_create() override { puts("on_create()"); }
-    void on_user_shutdown() override { puts("on_shutdown()"); }
+    void on_user_create() override { spdlog::info("on_create()"); }
+    void on_user_shutdown() override { spdlog::info("on_shutdown()"); }
 };
 
 auto orion::create_application() -> std::unique_ptr<Application>
