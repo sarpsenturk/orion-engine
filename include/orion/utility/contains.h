38f52ef2281dee94dd24474967c1b60c3bbbc918
sarpsenturk <sarpsenturk38@gmail.com>
2022-07-16 22:53:42 +0300

    Create utility contains & contains_v

---
+++ include/orion/utility/contains.h
@@ -0,0 +1,16 @@
+#ifndef ORION_ENGINE_CONTAINS_H
+#define ORION_ENGINE_CONTAINS_H
+
+#include <type_traits>
+
+namespace orion::detail
+{
+    template<typename T, typename... Ts>
+    struct contains : std::disjunction<std::is_same<T, Ts>...> {
+    };
+
+    template<typename T, typename... Ts>
+    constexpr bool contains_v = contains<T, Ts...>::value;
+} // namespace orion::detail
+
+#endif // ORION_ENGINE_CONTAINS_H
