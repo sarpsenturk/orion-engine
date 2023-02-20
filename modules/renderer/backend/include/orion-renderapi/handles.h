5179ba9398653c91ba48af486dad846f098f34b8
sarpsenturk <sarpsenturk38@gmail.com>
2023-02-20 15:31:05 +0300

    Create handles.h to contain renderer handles - Add SwapchainHandle

---
+++ modules/renderer/backend/include/orion-renderapi/handles.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "orion-core/handle.h"
+
+/*
+ * All handles to be used in the render backend should be defined here.
+ * This is to ensure we don't get odr violations.
+ */
+
+namespace orion
+{
+    ORION_DEFINE_HANDLE(SwapchainHandle);
+}
