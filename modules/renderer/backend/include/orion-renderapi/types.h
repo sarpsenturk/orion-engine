1296fec26e2ed6067900397f0984f8a8a6c3f5a1
sarpsenturk <sarpsenturk38@gmail.com>
2023-02-20 15:31:43 +0300

    Add orion::Format enum for renderer formats

--- modules/renderer/backend/include/orion-renderapi/types.h
+++ modules/renderer/backend/include/orion-renderapi/types.h
@@ -1,11 +1,24 @@
 #pragma once
 
-#include <cstdint>            // std::uint32_t
-#include <orion-utils/enum.h> // orion::enum_traits
-#include <string>             // std::string
+#include <cstdint>                     // std::uint32_t
+#include <orion-math/vector/vector2.h> // orion::math::Vector2
+#include <string>                      // std::string
 
 namespace orion
 {
+    enum class Format {
+        B8G8R8A8_SRGB
+    };
+
+    constexpr auto to_string(Format format) noexcept -> const char*
+    {
+        switch (format) {
+            case Format::B8G8R8A8_SRGB:
+                return "B8G8R8A8_SRGB";
+        }
+        return "Unknown format";
+    }
+
     enum class PhysicalDeviceType {
         Other,
         Integrated,
@@ -14,29 +27,32 @@ namespace orion
         CPU
     };
 
-    template<>
-    struct enum_traits<PhysicalDeviceType> : default_enum_traits<PhysicalDeviceType> {
-        static constexpr auto to_string(PhysicalDeviceType type) noexcept
-        {
-            switch (type) {
-                case PhysicalDeviceType::Other:
-                    break;
-                case PhysicalDeviceType::Integrated:
-                    return "Integrated";
-                case PhysicalDeviceType::Discrete:
-                    return "Discrete";
-                case PhysicalDeviceType::Virtual:
-                    return "Virtual";
-                case PhysicalDeviceType::CPU:
-                    return "CPU";
-            }
-            return "Other";
+    constexpr auto to_string(PhysicalDeviceType type) noexcept
+    {
+        switch (type) {
+            case PhysicalDeviceType::Other:
+                break;
+            case PhysicalDeviceType::Integrated:
+                return "Integrated";
+            case PhysicalDeviceType::Discrete:
+                return "Discrete";
+            case PhysicalDeviceType::Virtual:
+                return "Virtual";
+            case PhysicalDeviceType::CPU:
+                return "CPU";
         }
-    };
+        return "Other";
+    }
 
     struct PhysicalDeviceDesc {
         std::uint32_t index;
         PhysicalDeviceType type;
         std::string name;
+    };
+
+    struct SwapchainDesc {
+        std::uint32_t image_count;
+        Format image_format;
+        math::Vector2_u image_size;
     };
 } // namespace orion
