f891c2b36e2bde3159cabd6f183b031d11a85c19
sarpsenturk <sarpsenturk38@gmail.com>
2023-04-04 20:19:36 +0300

    Suppress unused return value warnings for fputs calls

--- modules/engine/include/orion-engine/entry_point.h
+++ modules/engine/include/orion-engine/entry_point.h
@@ -7,19 +7,19 @@
 #include <iterator>     // std::next
 #include <span>         // std::span
 
-#define ORION_MAIN(args)                                                 \
-    int user_main(std::span<const char* const> args);                    \
-    int main(int argc, const char* argv[])                               \
-    {                                                                    \
-        try {                                                            \
-            return user_main(std::span{argv, std::next(argv, argc)});    \
-        } catch (const orion::OrionException& e) {                       \
+#define ORION_MAIN(args)                                                   \
+    int user_main(std::span<const char* const> args);                      \
+    int main(int argc, const char* argv[])                                 \
+    {                                                                      \
+        try {                                                              \
+            return user_main(std::span{argv, std::next(argv, argc)});      \
+        } catch (const orion::OrionException& e) {                         \
             const auto what = fmt::format("[{}]: {}", e.type(), e.what()); \
-            std::fputs(what.c_str(), stderr);                            \
-            return e.return_code();                                      \
-        } catch (const std::exception& e) {                              \
-            std::fputs(e.what(), stderr);                                \
-            return -1;                                                   \
-        }                                                                \
-    }                                                                    \
+            (void)std::fputs(what.c_str(), stderr);                        \
+            return e.return_code();                                        \
+        } catch (const std::exception& e) {                                \
+            (void)std::fputs(e.what(), stderr);                            \
+            return -1;                                                     \
+        }                                                                  \
+    }                                                                      \
     int user_main([[maybe_unused]] std::span<const char* const> args)
