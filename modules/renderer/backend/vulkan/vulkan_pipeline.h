61a99a72ac71a02cd69629d228655fc5674ce48e
sarpsenturk <sarpsenturk38@gmail.com>
2023-04-23 20:12:28 +0300

    Add getters to VulkanPipeline

--- modules/renderer/backend/vulkan/vulkan_pipeline.h
+++ modules/renderer/backend/vulkan/vulkan_pipeline.h
@@ -10,6 +10,9 @@ namespace orion::vulkan
     public:
         VulkanPipeline(UniqueVkPipelineLayout pipeline_layout, UniqueVkPipeline pipeline);
 
+        [[nodiscard]] auto pipeline() const noexcept { return pipeline_.get(); }
+        [[nodiscard]] auto pipeline_layout() const noexcept { return pipeline_layout_.get(); }
+
     private:
         UniqueVkPipelineLayout pipeline_layout_;
         UniqueVkPipeline pipeline_;
