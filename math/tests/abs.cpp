5ac92295ef6bd23301cea03341d879afaeaefe8e
sarpsenturk <sarpsenturk38@gmail.com>
2023-06-17 17:49:04 +0300

    Add tests to anonymous namespace

--- math/tests/abs.cpp
+++ math/tests/abs.cpp
@@ -2,32 +2,23 @@
 
 #include <gtest/gtest.h>
 
-TEST(Abs, PositiveFloating)
+namespace
 {
-    EXPECT_EQ(orion::math::abs(15.f), 15.f);
-}
+    TEST(Abs, Floating)
+    {
+        EXPECT_EQ(orion::math::abs(15.f), 15.f);
+        EXPECT_EQ(orion::math::abs(-15.f), 15.f);
+    }
 
-TEST(Abs, NegativeFloating)
-{
-    EXPECT_EQ(orion::math::abs(-15.f), 15.f);
-}
+    TEST(Abs, Integral)
+    {
+        EXPECT_EQ(orion::math::abs(15), 15.0);
+        EXPECT_EQ(orion::math::abs(-15), 15.0);
+    }
 
-TEST(Abs, PositiveIntegral)
-{
-    EXPECT_EQ(orion::math::abs(15), 15.0);
-}
-
-TEST(Abs, NegativeIntegral)
-{
-    EXPECT_EQ(orion::math::abs(-15), 15.0);
-}
-
-TEST(Abs, PositiveZero)
-{
-    EXPECT_EQ(orion::math::abs(0.0), 0.0);
-}
-
-TEST(Abs, NegativeZero)
-{
-    EXPECT_EQ(orion::math::abs(-0.0), 0.0);
-}
+    TEST(Abs, Zero)
+    {
+        EXPECT_EQ(orion::math::abs(0.0), 0.0);
+        EXPECT_EQ(orion::math::abs(-0.0), 0.0);
+    }
+} // namespace
--- math/tests/sqrt.cpp
+++ math/tests/sqrt.cpp
@@ -2,19 +2,22 @@
 
 #include <gtest/gtest.h>
 
-TEST(Sqrt, IntegerResults)
+namespace
 {
-    EXPECT_EQ(orion::math::sqrt(4), 2.0);
-    EXPECT_EQ(orion::math::sqrt(16), 4.0);
-    EXPECT_EQ(orion::math::sqrt(64), 8.0);
-}
+    TEST(Sqrt, IntegerResults)
+    {
+        EXPECT_EQ(orion::math::sqrt(4), 2.0);
+        EXPECT_EQ(orion::math::sqrt(16), 4.0);
+        EXPECT_EQ(orion::math::sqrt(64), 8.0);
+    }
 
-TEST(Sqrt, FloatingResults)
-{
-    EXPECT_DOUBLE_EQ(orion::math::sqrt(2), 1.4142135623730950488);
-}
+    TEST(Sqrt, FloatingResults)
+    {
+        EXPECT_DOUBLE_EQ(orion::math::sqrt(2), 1.4142135623730950488);
+    }
 
-TEST(Sqrt, Zero)
-{
-    EXPECT_EQ(orion::math::sqrt(0), 0);
-}
+    TEST(Sqrt, Zero)
+    {
+        EXPECT_EQ(orion::math::sqrt(0), 0);
+    }
+} // namespace
