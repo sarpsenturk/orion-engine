d0bd8f49a1cfa2cfd737793f57d1aa3d79a2d594
sarpsenturk <sarpsenturk38@gmail.com>
2023-08-16 04:38:49 +0300

    Cleanup cmake files - Remove orion-base target and set whats needed globally - Change precompiled headers to be reused - Move platform detection into presets

---
+++ pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <utility>
+#include <version>
+#include <memory>
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <array>
+#include <span>
+#include <vector>
+#include <algorithm>
+#include <type_traits>
+#include <ranges>
