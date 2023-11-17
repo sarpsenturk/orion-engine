a3b7629af3c1c215257da8d10748f3884fd9397d
sarpsenturk <sarpsenturk38@gmail.com>
2023-11-17 14:30:55 +0300

    Simplify basic shader

--- assets/shaders/basic.ps.hlsl
+++ assets/shaders/basic.ps.hlsl
@@ -1,12 +1,8 @@
 struct PsInput {
    float4 color : COLOR;
-   float2 uv : TEXCOORD;
 };
-
-Texture2D texture : register(t0);
-SamplerState texture_sampler : register(s0);
 
 float4 ps_main(PsInput input) : SV_Target
 {
-   return texture.Sample(texture_sampler, input.uv) * input.color;
+   return input.color;
 }
--- assets/shaders/basic.vs.hlsl
+++ assets/shaders/basic.vs.hlsl
@@ -1,24 +1,17 @@
 struct VsInput {
    float3 position : POSITION;
    float4 color : COLOR;
-   float2 uv : TEXCOORD;
 };
 
 struct VsOutput {
    float4 position : SV_Position;
    float4 color : COLOR;
-   float2 uv : TEXCOORD;
-};
-
-cbuffer Scene {
-    float4x4 view_proj;
 };
 
 VsOutput vs_main(VsInput input)
 {
    VsOutput output;
-   output.position = mul(float4(input.position, 1.0), view_proj);
+   output.position = float4(input.position, 1.0);
    output.color = input.color;
-   output.uv = input.uv;
    return output;
 }
