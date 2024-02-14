6762d0c194f99f4ad380f67a4b45ff01020e8bf6
sarpsenturk <sarpsenturk38@gmail.com>
2024-02-14 21:31:25 +0300

    Set platform at top level CMakeLists.txt

--- shader-compiler/main.cpp
+++ shader-compiler/main.cpp
@@ -8,70 +8,78 @@
 
 int main(int argc, const char* argv[])
 {
-    auto cli_app = CLI::App{"Command line interface for the Orion Shader Compiler", "orion-shader-compiler"};
+    try {
+        auto cli_app = CLI::App{"Command line interface for the Orion Shader Compiler", "orion-shader-compiler"};
 
-    std::string output;
-    cli_app.add_option("-o,--output", output, "Output filename");
+        std::string output;
+        cli_app.add_option("-o,--output", output, "Output filename");
 
-    std::string entry_point;
-    cli_app.add_option("-e,--entry", entry_point, "Entry point name")
-        ->required();
+        std::string entry_point;
+        cli_app.add_option("-e,--entry", entry_point, "Entry point name")
+            ->required();
 
-    std::string target;
-    cli_app.add_option("-t,--target", target, "Target profile")
-        ->required();
+        std::string target;
+        cli_app.add_option("-t,--target", target, "Target profile")
+            ->required();
 
-    bool spirv = false;
-    bool dxil = false;
-    {
-        auto* ir_group = cli_app.add_option_group("IL Target", "Intermediate language target");
-        ir_group->require_option(1);
-        ir_group->add_flag("--spirv", spirv);
-        ir_group->add_flag("--dxil", dxil);
-    }
-
-    bool debug = false;
-    cli_app.add_flag("-d,--debug", debug, "Enable debug information");
-
-    std::string input;
-    cli_app.add_option("input", input, "Input file")
-        ->required()
-        ->check(CLI::ExistingFile);
-
-    CLI11_PARSE(cli_app, argc, argv)
-
-    const auto compile_desc = orion::ShaderCompileDesc{
-        .filename = input,
-        .entry_point = entry_point,
-        .target_profile = target,
-        .shader_il = spirv ? orion::ShaderIL::SpirV : orion::ShaderIL::DXIL,
-        .debug = debug,
-    };
-    auto compile_result = orion::shader_compiler::compile_file(input, compile_desc);
-
-    if (!compile_result) {
-        return static_cast<int>(compile_result.error().error);
-    }
-
-    auto write_output = [&compile_success = compile_result.value()](std::ostream& outstream) {
-        for (const char byte : compile_success.object) {
-            outstream << byte;
+        bool spirv = false;
+        bool dxil = false;
+        {
+            auto* ir_group = cli_app.add_option_group("IL Target", "Intermediate language target");
+            ir_group->require_option(1);
+            ir_group->add_flag("--spirv", spirv);
+            ir_group->add_flag("--dxil", dxil);
         }
-        return outstream.good();
-    };
 
-    bool output_good = false;
-    if (output.empty()) {
-        output_good = write_output(std::cout);
-    } else {
-        auto outfile = std::ofstream{output, std::ios::out | std::ios::binary | std::ios::trunc};
-        output_good = write_output(outfile);
+        bool debug = false;
+        cli_app.add_flag("-d,--debug", debug, "Enable debug information");
+
+        std::string input;
+        cli_app.add_option("input", input, "Input file")
+            ->required()
+            ->check(CLI::ExistingFile);
+
+        CLI11_PARSE(cli_app, argc, argv)
+
+        const auto compile_desc = orion::ShaderCompileDesc{
+            .filename = input,
+            .entry_point = entry_point,
+            .target_profile = target,
+            .shader_il = spirv ? orion::ShaderIL::SpirV : orion::ShaderIL::DXIL,
+            .debug = debug,
+        };
+        auto compile_result = orion::shader_compiler::compile_file(input, compile_desc);
+
+        if (!compile_result) {
+            return static_cast<int>(compile_result.error().error);
+        }
+
+        auto write_output = [&compile_success = compile_result.value()](std::ostream& outstream) {
+            for (const char byte : compile_success.object) {
+                outstream << byte;
+            }
+            return outstream.good();
+        };
+
+        bool output_good = false;
+        if (output.empty()) {
+            output_good = write_output(std::cout);
+        } else {
+            auto outfile = std::ofstream{output, std::ios::out | std::ios::binary | std::ios::trunc};
+            output_good = write_output(outfile);
+        }
+
+        if (!output_good) {
+            std::cerr << "Failed to create_write output\n";
+            return 1;
+        }
+
+        return 0;
     }
-
-    if (!output_good) {
-        std::cerr << "Failed to create_write output\n";
-        return 1;
+    catch (const std::exception& exception) {
+        std::cerr << exception.what();
     }
-
-    return 0;
+    catch (...) {
+        std::cerr << "Unknown exception throw\n";
+    }
 }
--- utils/tests/bitflag.cpp
+++ utils/tests/bitflag.cpp
@@ -4,19 +4,19 @@
 
 #include <algorithm>
 
+enum class MyEnum : std::uint8_t {
+    None = 0,
+    First = 0x1,
+    Second = 0x2,
+    Third = 0x4
+};
+
+template<>
+struct orion::enum_bitwise_enabled<MyEnum> : std::true_type {
+};
+
 namespace
 {
-    enum class MyEnum : std::uint8_t {
-        None = 0,
-        First = 0x1,
-        Second = 0x2,
-        Third = 0x4
-    };
-
-    template<>
-    struct orion::enum_bitwise_enabled<MyEnum> : std::true_type {
-    };
-
     TEST(Bitwise, Enabled)
     {
         static_assert(orion::bitwise_enum<MyEnum>);
