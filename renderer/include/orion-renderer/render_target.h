854ee4db188a4afecb29f8f79f997e5dcfa821f6
sarpsenturk <sarpsenturk38@gmail.com>
2024-05-13 21:01:59 +0300

    Add support for unowned images when creating RenderTargets

--- renderer/include/orion-renderer/render_target.h
+++ renderer/include/orion-renderer/render_target.h
@@ -1,34 +1,42 @@
 #pragma once
 
+#include "orion-renderapi/defs.h"
 #include "orion-renderapi/device_resource.h"
 
 #include "orion-math/vector/vector2.h"
 
+#include <variant>
+
 namespace orion
 {
+    using RenderTargetImage = std::variant<ImageHandle, UniqueImage>;
+    ImageHandle get_image_handle(const RenderTargetImage& render_target_image);
+
     class RenderTarget
     {
     public:
-        RenderTarget(UniqueImage image, UniqueImageView image_view, UniqueRenderPass render_pass, UniqueFramebuffer framebuffer);
+        RenderTarget(RenderTargetImage image, UniqueImageView image_view, UniqueRenderPass render_pass, UniqueFramebuffer framebuffer);
 
-        [[nodiscard]] ImageHandle image() const noexcept { return image_.get(); }
-        [[nodiscard]] ImageViewHandle image_view() const noexcept { return image_view_.get(); }
         [[nodiscard]] FramebufferHandle framebuffer() const noexcept { return framebuffer_.get(); }
         [[nodiscard]] RenderPassHandle render_pass() const noexcept { return render_pass_.get(); }
 
     private:
-        UniqueImage image_;
+        RenderTargetImage image_;
         UniqueImageView image_view_;
         UniqueRenderPass render_pass_;
         UniqueFramebuffer framebuffer_;
     };
 
+    struct RenderTargetDesc {
+        Vector2_u size;
+        ImageUsageFlags image_usage = ImageUsageFlags::ColorAttachment;
+        ImageLayout initial_layout = ImageLayout::Undefined;
+        ImageLayout final_layout = ImageLayout::General;
+    };
+
     // Forward declare
     class RenderDevice;
 
-    struct RenderTargetDesc {
-        Vector2_u size;
-    };
-
+    RenderTarget create_render_target(RenderDevice* device, ImageHandle image, const RenderTargetDesc& desc);
     RenderTarget create_render_target(RenderDevice* device, const RenderTargetDesc& desc);
 } // namespace orion
--- renderer/src/render_target.cpp
+++ renderer/src/render_target.cpp
@@ -1,19 +1,69 @@
 #include "orion-renderer/render_target.h"
 
 #include "orion-renderapi/render_device.h"
+#include "orion-renderapi/swapchain.h"
 
 #include "orion-math/vector/vector3.h"
+
+#include "orion-utils/callable.h"
 
 #include <utility>
 
 namespace orion
 {
-    RenderTarget::RenderTarget(UniqueImage image, UniqueImageView image_view, UniqueRenderPass render_pass, UniqueFramebuffer framebuffer)
+    namespace
+    {
+        RenderTarget make_render_target(RenderDevice* device, RenderTargetImage image, const RenderTargetDesc& desc)
+        {
+            auto image_view = device->create_image_view({
+                .image = get_image_handle(image),
+                .type = ImageViewType::View2D,
+                .format = Format::B8G8R8A8_Srgb,
+            });
+
+            auto render_pass = device->create_render_pass({
+                .color_attachments = {{
+                    AttachmentDesc{
+                        .format = Format::B8G8R8A8_Srgb,
+                        .load_op = AttachmentLoadOp::Clear,
+                        .store_op = AttachmentStoreOp::Store,
+                        .initial_layout = desc.initial_layout,
+                        .layout = ImageLayout::ColorAttachment,
+                        .final_layout = desc.final_layout,
+                    },
+                }},
+            });
+
+            auto framebuffer = device->create_framebuffer({
+                .render_pass = render_pass,
+                .image_views = {&image_view, 1},
+                .size = desc.size,
+            });
+
+            return RenderTarget{std::move(image), device->to_unique(image_view), device->to_unique(render_pass), device->to_unique(framebuffer)};
+        }
+    } // namespace
+
+    ImageHandle get_image_handle(const RenderTargetImage& render_target_image)
+    {
+        const auto overload = Overload{
+            [](const UniqueImage& image) { return image.get(); },
+            [](ImageHandle image) { return image; },
+        };
+        return std::visit(overload, render_target_image);
+    }
+
+    RenderTarget::RenderTarget(RenderTargetImage image, UniqueImageView image_view, UniqueRenderPass render_pass, UniqueFramebuffer framebuffer)
         : image_(std::move(image))
         , image_view_(std::move(image_view))
         , render_pass_(std::move(render_pass))
         , framebuffer_(std::move(framebuffer))
     {
+    }
+
+    RenderTarget create_render_target(RenderDevice* device, ImageHandle image, const RenderTargetDesc& desc)
+    {
+        return make_render_target(device, image, desc);
     }
 
     RenderTarget create_render_target(RenderDevice* device, const RenderTargetDesc& desc)
@@ -23,35 +73,9 @@ namespace orion
             .format = Format::B8G8R8A8_Srgb,
             .size = vec3(desc.size, 1u),
             .tiling = ImageTiling::Optimal,
-            .usage = ImageUsageFlags::ColorAttachment | ImageUsageFlags::SampledImage,
+            .usage = desc.image_usage,
             .host_visible = false,
         });
-
-        auto image_view = device->create_image_view({
-            .image = image,
-            .type = ImageViewType::View2D,
-            .format = Format::B8G8R8A8_Srgb,
-        });
-
-        auto render_pass = device->create_render_pass({
-            .color_attachments = {{
-                AttachmentDesc{
-                    .format = Format::B8G8R8A8_Srgb,
-                    .load_op = AttachmentLoadOp::Clear,
-                    .store_op = AttachmentStoreOp::Store,
-                    .initial_layout = ImageLayout::Undefined,
-                    .layout = ImageLayout::ColorAttachment,
-                    .final_layout = ImageLayout::ShaderReadOnly,
-                },
-            }},
-        });
-
-        auto framebuffer = device->create_framebuffer({
-            .render_pass = render_pass,
-            .image_views = {&image_view, 1},
-            .size = desc.size,
-        });
-
-        return RenderTarget{device->to_unique(image), device->to_unique(image_view), device->to_unique(render_pass), device->to_unique(framebuffer)};
+        return make_render_target(device, device->to_unique(image), desc);
     }
 } // namespace orion
