cd10972cd6582fdc695557cb43c137a054b6e10d
sarpsenturk <sarpsenturk38@gmail.com>
2025-12-10 03:21:25 +0300

    Add platform input layer

---
+++ include/orion/platform/input.hpp
@@ -0,0 +1,165 @@
+#pragma once
+
+namespace orion
+{
+    enum class KeyCode {
+        Space = 32,
+        Apostrophe = 39,
+
+        Comma = 44,
+        Minus = 45,
+        Period = 46,
+        Slash = 47,
+
+        K0 = 48,
+        K1,
+        K2,
+        K3,
+        K4,
+        K5,
+        K6,
+        K7,
+        K8,
+        K9,
+
+        Semicolon = 59,
+        Equal = 61,
+
+        A = 65,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
+        J,
+        K,
+        L,
+        M,
+        N,
+        O,
+        P,
+        Q,
+        R,
+        S,
+        T,
+        U,
+        V,
+        W,
+        X,
+        Y,
+        Z,
+
+        LeftBracket = 91,
+        Backslash = 92,
+        RightBracket = 93,
+        GraveAccent = 96,
+
+        World1 = 161,
+        World2 = 162,
+
+        Escape = 256,
+        Enter = 257,
+        Tab = 258,
+        Backspace = 259,
+        Insert = 260,
+        Delete = 261,
+
+        RightArrow = 262,
+        LeftArrow = 263,
+        DownArrow = 264,
+        UpArrow = 265,
+
+        PageUp = 266,
+        PageDown = 267,
+
+        Home = 268,
+        End = 269,
+
+        CapsLock = 280,
+        ScrollLock = 281,
+        NumLock = 282,
+
+        PrintScreen = 283,
+        Pause = 284,
+
+        F1 = 290,
+        F2,
+        F3,
+        F4,
+        F5,
+        F6,
+        F7,
+        F8,
+        F9,
+        F10,
+        F11,
+        F12,
+        F13,
+        F14,
+        F15,
+        F16,
+        F17,
+        F18,
+        F19,
+        F20,
+        F21,
+        F22,
+        F23,
+        F24,
+        F25,
+
+        Num0 = 320,
+        Num1,
+        Num2,
+        Num3,
+        Num4,
+        Num5,
+        Num6,
+        Num7,
+        Num8,
+        Num9,
+        NumDecimal = 330,
+        NumDivide,
+        NumMultiply,
+        NumSubtract,
+        NumAdd,
+        NumEnter,
+        NumEqual,
+
+        LeftShift = 340,
+        LeftControl = 341,
+        LeftAlt = 342,
+        LeftSuper = 343,
+        RightShift = 344,
+        RightControl = 345,
+        RightAlt = 346,
+        RightSuper = 347,
+        Menu = 348,
+    };
+
+    enum class MouseButton {
+        Button1 = 0,
+        Button2,
+        Button3,
+        Button4,
+        Button5,
+        Button6,
+        Button7,
+        Button8,
+
+        Left = Button1,
+        Right = Button2,
+        Middle = Button3,
+    };
+
+    struct Window;
+    bool platform_input_key_pressed(Window* window, KeyCode key);
+    bool platform_input_key_released(Window* window, KeyCode key);
+    void platform_input_cursor_position(Window* window, double* xpos, double* ypos);
+    bool platform_input_mouse_button_pressed(Window* window, MouseButton button);
+    bool platform_input_mouse_button_released(Window* window, MouseButton button);
+    void platform_input_scroll_delta(Window* window, double* xdelta, double* ydelta);
+} // namespace orion
--- src/platform/platform_glfw.cpp
+++ src/platform/platform_glfw.cpp
@@ -1,3 +1,4 @@
+#include "orion/platform/input.hpp"
 #include "orion/platform/window.hpp"
 
 #include "orion/assert.hpp"
@@ -5,17 +6,50 @@
 
 #include <GLFW/glfw3.h>
 
+#include <array>
+
 namespace orion
 {
     struct Window {
         GLFWwindow* window;
         int width;
         int height;
+
+        std::array<int, GLFW_KEY_LAST> key_states;
+        std::array<int, GLFW_MOUSE_BUTTON_LAST> mouse_button_states;
+        double cursor_xpos, cursor_ypos;
+        double scroll_xoffset, scroll_yoffet;
     };
 
     static void glfw_error_callback(int code, const char* description)
     {
         ORION_CORE_LOG_ERROR("GLFW error ({}): {}", code, description);
+    }
+
+    static void glfw_key_callback(GLFWwindow* window, int key, int /*scancode*/, int action, int /*mods*/)
+    {
+        auto* impl = static_cast<Window*>(glfwGetWindowUserPointer(window));
+        impl->key_states[key] = action;
+    }
+
+    static void glfw_cursor_pos_callback(GLFWwindow* window, double xpos, double ypos)
+    {
+        auto* impl = static_cast<Window*>(glfwGetWindowUserPointer(window));
+        impl->cursor_xpos = xpos;
+        impl->cursor_ypos = ypos;
+    }
+
+    static void glfw_mouse_button_callback(GLFWwindow* window, int button, int action, int /*mods*/)
+    {
+        auto* impl = static_cast<Window*>(glfwGetWindowUserPointer(window));
+        impl->mouse_button_states[button] = action;
+    }
+
+    static void glfw_scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+    {
+        auto* impl = static_cast<Window*>(glfwGetWindowUserPointer(window));
+        impl->scroll_xoffset = xoffset;
+        impl->scroll_yoffet = yoffset;
     }
 
     bool platform_init()
@@ -48,6 +82,15 @@ namespace orion
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         window->window = glfwCreateWindow(width, height, title, nullptr, nullptr);
 
+        // Set window user pointer
+        glfwSetWindowUserPointer(window->window, window);
+
+        // Setup event callbacks
+        glfwSetKeyCallback(window->window, glfw_key_callback);
+        glfwSetCursorPosCallback(window->window, glfw_cursor_pos_callback);
+        glfwSetMouseButtonCallback(window->window, glfw_mouse_button_callback);
+        glfwSetScrollCallback(window->window, glfw_scroll_callback);
+
         // Get created framebuffer dimensions
         glfwGetFramebufferSize(window->window, &window->width, &window->height);
         if (window->width == width && window->height == height) {
@@ -72,6 +115,10 @@ namespace orion
     void platform_window_poll_events(Window* window)
     {
         ORION_ASSERT(window != nullptr, "platform_window_poll_events() expected window != nullptr");
+        std::memset(&window->key_states, GLFW_RELEASE, sizeof(window->key_states));
+        std::memset(&window->mouse_button_states, GLFW_RELEASE, sizeof(window->mouse_button_states));
+        window->scroll_xoffset = 0.0;
+        window->scroll_yoffet = 0.0;
         glfwPollEvents();
     }
 
@@ -79,5 +126,38 @@ namespace orion
     {
         ORION_ASSERT(window != nullptr, "platform_window_should_close() expected window != nullptr");
         return glfwWindowShouldClose(window->window);
+    }
+
+    bool platform_input_key_pressed(Window* window, KeyCode key)
+    {
+        return window->key_states[static_cast<int>(key)] == GLFW_PRESS ||
+               window->key_states[static_cast<int>(key)] == GLFW_REPEAT;
+    }
+
+    bool platform_input_key_released(Window* window, KeyCode key)
+    {
+        return window->key_states[static_cast<int>(key)] == GLFW_RELEASE;
+    }
+
+    void platform_input_cursor_position(Window* window, double* xpos, double* ypos)
+    {
+        *xpos = window->cursor_xpos;
+        *ypos = window->cursor_ypos;
+    }
+
+    bool platform_input_mouse_button_pressed(Window* window, MouseButton button)
+    {
+        return window->mouse_button_states[static_cast<int>(button)] == GLFW_PRESS;
+    }
+
+    bool platform_input_mouse_button_released(Window* window, MouseButton button)
+    {
+        return window->mouse_button_states[static_cast<int>(button)] == GLFW_RELEASE;
+    }
+
+    void platform_input_scroll_delta(Window* window, double* xdelta, double* ydelta)
+    {
+        *xdelta = window->scroll_xoffset;
+        *ydelta = window->scroll_yoffet;
     }
 } // namespace orion
